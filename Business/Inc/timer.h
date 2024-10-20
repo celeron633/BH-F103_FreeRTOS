3bbf5955d5715d6a5b09aad0b4f143bbc6ad3ff3
celeron633 <dengxionghui@hotmail.com>
2024-10-20 23:02:48 +0800

    timer: finish oled display part && optimize RTOS heap size

--- Business/Inc/timer.h
+++ Business/Inc/timer.h
@@ -1,6 +1,9 @@
 #ifndef __TIMER_H__
 #define __TIMER_H__
 
+#include "i2c.h"
+#include "oled.h"
+
 #include <stdio.h>
 
 void TimerLogic(void *arg);
--- Business/Src/timer.c
+++ Business/Src/timer.c
@@ -1,6 +1,29 @@
 #include "timer.h"
 
+#include "FreeRTOS.h"
+#include "task.h"
+
+
+extern I2C_HandleTypeDef hi2c2;
+
+static void DisplayInit()
+{
+    OLED_ConfigDisplay(&hi2c2, 0x78);
+    (void)OLED_InitDisplay();
+    OLED_NewFrame();
+    OLED_ShowString(0, 0, "TIMER INIT....");
+    OLED_ShowFrame();
+}
+
 void TimerLogic(void *arg)
 {
-    
+    (void)arg;
+
+    DisplayInit();
+    vTaskDelay(500);
+
+    while (1)
+    {
+        
+    }
 }
